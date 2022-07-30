c3e0805629e7a3495337fade20741de6b0ca5577
Marco Ferretti <mferretti93@gmail.com>
2022-07-30 11:59:02 +0200

    unoptimize images

--- next.config.js
+++ next.config.js
@@ -5,5 +5,23 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   images: {
     domains: ['cdn.chec.io'],
+    loader: 'custom',
   },
+  
 });
+
+// module.exports = withBundleAnalyzer({
+//   images: {
+//     domains: ['cdn.chec.io'],
+//     loader: 'custom',
+//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+//   },
+//   env: {
+//     nextImageExportOptimizer_imageFolderPath: 'public/images',
+//     nextImageExportOptimizer_exportFolderPath: 'out',
+//     nextImageExportOptimizer_quality: '75',
+//     nextImageExportOptimizer_storePicturesInWEBP: 'true',
+//     nextImageExportOptimizer_generateAndUseBlurImages: 'true',
+//   },
+// });
